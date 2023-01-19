194559f7ad82705175a85ef17002ec39759dac11
ponjeeva J <ponjeevajeeva2002@gmail.com>
2023-01-19 18:37:32 +0530

    commit

---
+++ style.js
@@ -0,0 +1,32 @@
+// function decleration
+// function  cal (birth){
+//     return 2022-birth;
+// }
+// const age1=cal(2002);
+// console.log(age1);
+// function exprection
+// const cal1 =function(birth){
+//     return 2022-birth;
+// }
+// const age2=cal1(2002);
+// console.log(age2);
+// arrow function in oneline code
+// const age3= birth=>2022-birth;
+// const cal=age3(2002);
+// console.log(cal);
+// arrow function more than one line code
+// const age =birth=>{
+//     const year=2022-birth;
+//     const ret=65-year;
+//     return ret;
+// }
+// const ans=age(2002);
+// console.log(ans);
+// arrow function with different parameter
+// const age =(birth,name)=>{
+//     const year=2022-birth;
+//     const ret=65-year;
+//     return `i am ${name} i will reter in ${ret} years `;
+// }
+// const ans=age(2002,'jeeva');
+// console.log(ans);
